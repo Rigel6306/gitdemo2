8b2fe3af7ab0afe35d875a870fddfd0543564a55
charitha <charitha1@live.com>
2024-01-20 17:28:59 +0530

    changes made on the new branch

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 console.log("app js file is being developed")
 
 console.log("master branch development keep going")
+
+console.log("master branch development keep going")
+console.log("master branch development keep going")
+console.log("master branch development keep going")
+console.log("master branch development keep going")
+console.log("master branch development keep going")
+console.log("master branch development keep going")
